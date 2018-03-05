8199d546f22679e6839dd43e823350991527e9c5
yourzeromax <yourzeromax@163.com>
2018-03-05 15:13:15 +0800

    yourzeromax init

--- sw.js
+++ sw.js
@@ -137,7 +137,6 @@ self.addEventListener('fetch', event => {
         .then(resp => resp || fetched)
         .catch(_ => caches.match('offline.html'))
     );
-
     // Update the cache with the version we fetched (only for ok status)
     event.waitUntil(
       Promise.all([fetchedCopy, caches.open(RUNTIME)])
